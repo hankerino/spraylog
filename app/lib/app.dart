75ec424324c7ffb58d005ace9bf9dea1fac17d11
Henry Quinones <hankquin@gmail.com>
2026-07-30 14:11:50 +0200

    feat: add routing and application skeleton

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'features/auth/auth_screen.dart';
+import 'features/home/home_screen.dart';
+import 'features/onboarding/onboarding_screen.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/auth',
+      builder: (context, state) => const AuthScreen(),
+    ),
+    GoRoute(
+      path: '/onboarding',
+      builder: (context, state) => const OnboardingScreen(),
+    ),
+  ],
+);
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'SprayLog',
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('SprayLog'),
-        ),
-      ),
+      routerConfig: _router,
     );
   }
 }
